b1665f2baf92537ffcf4c746e44523a060561c85
Akkas Singh <akkassingh@gmail.com>
2024-04-26 19:09:17 -0400

    building web server.

---
+++ README.md
@@ -0,0 +1,8 @@
+this project serves as a lab to revisit my node skills.
+
+run `npm i` to install dependencies.
+
+`npm run server` to run app with nodemon or
+
+`npm start`
+
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const { createServer } = require('http');
 
 createServer((req, res) => {
     res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Hello World!");
+    res.end(
+        `
+        <!DOCTYPE html>
+        <html>
+            <body>
+            <h1>Hello World!<h1>
+            <p>${req.method} request made for ${req.url}
+        `
+    );
 }).listen(3000);
 
 console.log("Web server listening on port 3000");

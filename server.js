3817db6c4a163a64ab268f81c3b534bf7ae8adb9
Akkas Singh <akkassingh@gmail.com>
2024-04-26 19:38:56 -0400

    serving json data

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 const { createServer } = require("http");
 const { createReadStream } = require("fs");
-const { type } = require("os");
-const { send } = require("process");
+const data = require("./sample_data.json");
 
-const sendFile = (res, staus, type, file) => {
-  res.writeHead(200, { "Content-Type": type });
-  createReadStream(file).pipe(res);
-};
 
 createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      return sendFile(res, 200, "text/html", "./homepage.html");
-    case "/assets/sample.jpeg":
-      return sendFile(res, 200, "img/jpeg", "./assets/sample.jpeg");
-    case "/styles.css":
-      return sendFile(res, 200, "img/css", "./assets/styles.css");
-    default:
-      return sendFile(res, 404, "text/html", "./404.html");
-  }
+    res.writeHead(200, { "Content-Type": "text/json" });
+    if(req.url.toLocaleLowerCase() === "/sample") {
+        data.menu.id="samplefile";
+    }
+    res.end(JSON.stringify(data));
 }).listen(3000);
 
 console.log("Web server listening on port 3000");

7c72b1a297d64bd634282e8ff0a7f73ee7375df6
Akkas Singh <akkassingh@gmail.com>
2024-04-26 19:30:12 -0400

    creating file server

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const { createServer } = require('http');
+const { createServer } = require("http");
+const { createReadStream } = require("fs");
+const { type } = require("os");
+const { send } = require("process");
+
+const sendFile = (res, staus, type, file) => {
+  res.writeHead(200, { "Content-Type": type });
+  createReadStream(file).pipe(res);
+};
 
 createServer((req, res) => {
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end(
-        `
-        <!DOCTYPE html>
-        <html>
-            <body>
-            <h1>Hello World!<h1>
-            <p>${req.method} request made for ${req.url}
-        `
-    );
+  switch (req.url) {
+    case "/":
+      return sendFile(res, 200, "text/html", "./homepage.html");
+    case "/assets/sample.jpeg":
+      return sendFile(res, 200, "img/jpeg", "./assets/sample.jpeg");
+    case "/styles.css":
+      return sendFile(res, 200, "img/css", "./assets/styles.css");
+    default:
+      return sendFile(res, 404, "text/html", "./404.html");
+  }
 }).listen(3000);
 
 console.log("Web server listening on port 3000");

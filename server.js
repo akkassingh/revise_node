7edeca774a59ea7011e9f99a50a92736a6f87b64
Akkas Singh <akkassingh@gmail.com>
2024-04-28 14:07:51 -0400

    express post and get req

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-import parseArgs from "minimist";
-import Timer from "tiny-timer";
+import express from "express";
+import sample_data from './assets/sample_data.json' assert { type: "json" };
+import bodyParser from "body-parser";
+import fs from "fs";
 
-const { time } = parseArgs(process.argv);
+const app = express();
 
-if(!time){
-    throw new Error ("--- time is required");
+app.use("/", express.static("./assets"));
+app.get("/sample", (req, res) => {
+    res.json(sample_data);
+})
+
+app.post("/addsample", bodyParser.json(),(req, res) => {
+    sample_data.menu.popup.menuitem.push(req.body)
+    save();
+    res.json({
+        staus: "success",
+        data: sample_data
+    });
+})
+
+app.delete("/deletesample/:item", bodyParser.json(),(req, res) => {
+    sample_data.menu.popup.menuitem.filter !== req.params.item; 
+    save();
+    res.json({
+        staus: "success",
+        data: req.params.item
+    });
+})
+
+const save = () => {
+    fs.writeFile("./assets/sample_data.json", JSON.stringify(sample_data, null, 2), err => {
+        if(err){
+            throw err;
+        }
+    })
 }
 
-if(!parseInt(time)){
-    throw new Error ("--- time must be a number");
-}
-
-console.log(time);
-
-const timer = new Timer();
-timer.on("tick", () => console.log("tick"));
-timer.on("done", () => console.log("ticking is complete."));
-
-timer.start(time * 1000);
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+})

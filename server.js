65a3fedc8c7050f0f02f54327daf8cc134ba1e0e
Akkas Singh <akkassingh@gmail.com>
2024-04-28 14:32:05 -0400

    creating a websocket server

--- server.js
+++ server.js
@@ -2,49 +2,19 @@ import express from "express";
 import sample_data from './assets/sample_data.json' assert { type: "json" };
 import bodyParser from "body-parser";
 import fs from "fs";
+import { WebSocketServer } from "ws";
 
-const app = express();
 
-app.use("/", express.static("./assets"));
-app.get("/sample", (req, res) => {
-    res.json(sample_data);
+const wss = new WebSocketServer({
+    port: 3000
 })
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    console.log(`${req.method} request for ${req.url}`)
-    if(Object.keys(req.body).length){
-        console.log(req.body);
-    }
-    next();
-})
-
-app.post("/addsample", bodyParser.json(),(req, res) => {
-    sample_data.menu.popup.menuitem.push(req.body)
-    save();
-    res.json({
-        staus: "success",
-        data: sample_data
-    });
-})
-
-app.delete("/deletesample/:item", bodyParser.json(),(req, res) => {
-    sample_data.menu.popup.menuitem.filter !== req.params.item; 
-    save();
-    res.json({
-        staus: "success",
-        data: req.params.item
-    });
-})
-
-const save = () => {
-    fs.writeFile("./assets/sample_data.json", JSON.stringify(sample_data, null, 2), err => {
-        if(err){
-            throw err;
-        }
+wss.on('connection', (ws) => {
+    ws.on('close', () => {
+        console.log("socket disconnected.")
     })
-}
+    
+    console.log("new socket connected.")
+});
 
-app.listen(3000, () => {
-    console.log("server running on port 3000");
-})
+console.log("shat server running on port 3000");

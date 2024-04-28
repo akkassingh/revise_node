54c7644d374a2e7a7c7094c3af5734dc1b45cf6a
Akkas Singh <akkassingh@gmail.com>
2024-04-28 14:49:37 -0400

    introducing websockets

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-import express from "express";
-import sample_data from './assets/sample_data.json' assert { type: "json" };
-import bodyParser from "body-parser";
-import fs from "fs";
 import { WebSocketServer } from "ws";
-
 
 const wss = new WebSocketServer({
     port: 3000
 })
 
+let messages = [];
+
 wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message.toString())
+        messages.push(message.toString());
+        if(message.toString() == "exit")
+        {
+            ws.close()
+        } else {
+            wss.clients.forEach((client) => client.send(message.toString())
+        );
+        }
+    });
     ws.on('close', () => {
         console.log("socket disconnected.")
     })
     
     console.log("new socket connected.")
+    ws.send("welcome to live chat!");
+    if(messages.length){
+        ws.send("chat currently in session");
+        messages.forEach(message => ws.send(message.toString()))
+    }
 });
 
-console.log("shat server running on port 3000");
+console.log("Chat Server running on port 3000");

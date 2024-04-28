3622408a31159e901864e9b57143d96d4e778903
Akkas Singh <akkassingh@gmail.com>
2024-04-28 15:30:09 -0400

    socket.io implementation

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ run `npm i` to install dependencies.
 
 `npm start`
 
+for testing socket-client with socket server.
+    you need 2 terminal winows, one for the server and one to server the client.
+    run `npm start` to start the server.
+    run `npm start socket-client` to start the server.
--- server.js
+++ server.js
@@ -1,33 +1,18 @@
-import { WebSocketServer } from "ws";
+import {createServer} from 'http';
+import { Server } from 'socket.io';
 
-const wss = new WebSocketServer({
-    port: 3000
+const server = createServer().listen(3000);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log(`${io.engine.clientsCount} connections`);
+    socket.on('chat', (message) => {
+        console.log(`${socket.id}: ${message}`);
+        io.sockets.emit("message", message, socket.id);
+    });
+    socket.on("disconnect", () => {
+        console.log("disconnected", socket.id)
+    })
 })
 
-let messages = [];
-
-wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        console.log(message.toString())
-        messages.push(message.toString());
-        if(message.toString() == "exit")
-        {
-            ws.close()
-        } else {
-            wss.clients.forEach((client) => client.send(message.toString())
-        );
-        }
-    });
-    ws.on('close', () => {
-        console.log("socket disconnected.")
-    })
-    
-    console.log("new socket connected.")
-    ws.send("welcome to live chat!");
-    if(messages.length){
-        ws.send("chat currently in session");
-        messages.forEach(message => ws.send(message.toString()))
-    }
-});
-
-console.log("Chat Server running on port 3000");
+console.log("socket sever running on port 3000");
